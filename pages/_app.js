f3a25256df5734d4f9fe67e1d7fa2ee8b3ecc130
Alex Jones <ajwilt97@gmail.com>
2022-12-18 18:10:40 +0000

    Wrapped Layout in AppWrapper to allow use of AppReducer and relative functions aswell as CardContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import Layout from '../components/Layout'
+import { AppWrapper } from '../context/CardContext'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  return ( 
+    <AppWrapper>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppWrapper>
+  )
 }
 
 export default MyApp
