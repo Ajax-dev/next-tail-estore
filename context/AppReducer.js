e064130b194a166f9d269dc087ea8221d1b859af
Alex Jones <ajwilt97@gmail.com>
2022-12-18 18:02:47 +0000

    Add cases for AppReducer that can be used acrossed the App via useAppContext

--- context/AppReducer.js
+++ context/AppReducer.js
@@ -1,15 +1,62 @@
 export const initialState = {
     prices: [],
-    products: {}
+    products: {} //product key {[id]: value}
 }
 
 export const AppReducer = (state = initialState, action ) => {
     switch(action.type) {
-        case "add_product": { //[id] 
+        case "add_product" : { //[id] 
             return {
-                ...state
+                ...state,
+                products: {
+                    ...state.products,
+                    [action.value] : 1
+                }
+            }
+            
+        }
+
+        // creates whole new object with everything except something
+        case "remove_product" : { //receives [[id], [size], [newCount]]
+            return {
+                ...state,
+                products: Object.keys(state.products).reduce((acc, curr) => {
+                    if (curr !== action.value) {
+                        return { ...acc, [curr]: state.products[curr]}
+                    }
+                    return acc
+                }, {})
             }
         }
+
+        case "vary_count" : { // [id , newCount]
+            return {
+                ...state,
+                products: {
+                    ...state.products,
+                    [action.value[0]]: action.value[1],
+                    
+                }
+            }
+        }
+
+        case "load_items" : {
+            return {
+                ...state,
+                prices: action.value.prices,
+                products: action.value.products
+            }
+        }
+
+        case "set_prices" : {
+            return {
+                ...state,
+                prices: action.value
+            }
+        }
+
+        default:
+            return state
     }
 }
 
