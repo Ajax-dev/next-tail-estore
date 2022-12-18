76e7f2b74a82ede82a52aca04ff1ce6f43482993
Alex Jones <ajwilt97@gmail.com>
2022-12-18 18:07:08 +0000

    Add curried function addToBasket and debugged issue
    Issue with image not loading on its page (see previous commit)
    to do with trying to dynamically allocate string to link when pushing with
    router. Was using "'" instead of "`" and the ${variablename} inside meant
    the line was simply '/${variablename}' instead of '/product_id<idnum>'
    
    debugged using console logs of all of the constants set,
    added a check for undefined product.

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useAppContext } from '../../context/CardContext'
 
 export async function getServerSideProps(context) {
     const {params} = context
@@ -9,9 +10,30 @@ export async function getServerSideProps(context) {
 }
 
 export default function Description(props){
+    const { id: path } = props
+    const { state: { prices } = [], dispatch} = useAppContext()
+    const product = prices.filter(val => val.id === path.replace('/', ''))[0]
+    console.log("IM HERE")
+    console.log(props.product)
+    console.log(prices)
+    console.log(path)
+
+    if (product === undefined) { return <div className='pt-40 select-none grid place-items-center'>LOADING...</div> }
+
+    function addToBasket(prod) {
+        // curried function
+        return () => {
+            dispatch({
+                type: 'add_product',
+                value: prod.id
+            })
+        }
+    }
+
     return(
-        <div>
-            hello
+        <div className='mx-auto w-fit flex flex-wrap justify-center md:gap-6'>
+            <img src={product.product.images[0]} alt={product.product.id} styles={{maxHeight:'600px'}}/>
+            <button onClick={addToBasket(product)}>ADD TO BASKET</button>
         </div>
     )
 }
